687dc19d5c18f6c00474bd9efca40288f1abe33e
AlejandroSenior <51002654+AlejandroSenior@users.noreply.github.com>
2025-05-31 21:58:25 +0200

    Update fetch url

--- src/main.js
+++ src/main.js
@@ -45,7 +45,7 @@ const enviarArchivo = async () => {
     $dropzone.classList.add("dropzone-disabled");
 
     const response = await fetch(
-      "https://restful-checker-api.onrender.com/analyze",
+      "http://raspberrywl.duckdns.org:53127/analyze",
       {
         method: "POST",
         headers: {
